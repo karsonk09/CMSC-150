c49954d6d8700a55f1e610b264df3543d0c09390
Karson Richardson <karson.richardson@my.simpson.edu>
2017-01-10 21:49:46 +0000

    Edited to include lines from Wrinkle in Time

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,4 @@
 print("Hello there, my name is Karson")
 print("It was a dark and stormy night.")
-print("Suddenly a shot rang out")
+print("In her attic bedroom Margaret Murray, wrapped in an old patchwork quilt, sat on the")
+print("foot of her bed and watched the trees tossing in the frenzied lashing of the wind")
