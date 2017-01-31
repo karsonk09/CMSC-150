0617c61f2b8033003fc4ecac683d0e5c8cc5cdc2
SC\karson.richardson <karson.richardson@my.simpson.edu>
2017-01-31 15:55:47 -0600

    Finished Pirate Game

---
+++ Lab 04 - Camel/main.py
@@ -0,0 +1,137 @@
+import random
+
+
+# End sequence
+def game_over():
+    end = ("Would you like to play again? (Y/N) ")
+    if end == "Y":
+        main()
+    if end == "N":
+        print("GAME OVER")
+
+
+# Intro to game
+def main():
+    print("Welcome to Pirates!")
+    print("You have stolen gold and a boat from a local band of pirates in Havana.")
+    print("The pirates want their gold back and are chasing you down with their ships!")
+    print("You must travel across the Golden Sea to escape the pirates!")
+    print("Survive your ocean adventure and enjoy the spoils of your efforts.")
+
+
+
+
+    done = False
+
+    # Variables
+    player_hunger = 0
+    boat_condition = 0
+    miles_traveled = 0
+    dist_traveled_p = -20
+    food_reserves = 4
+
+    # Player choices
+    while not done:
+        print()
+        print("A. Eat from your reserves.")
+        print("B. Ahead half sail.")
+        print("C. Ahead full sail.")
+        print("D. Stop for the night.")
+        print("E. Status check.")
+        print("Q. Quit.")
+        choice = input("What is your choice? ")
+        print()
+
+        # Player chooses to quit
+        if choice.upper() == "Q":
+            done = True
+
+        # Player chooses to do a status check
+        elif choice.upper() == "E":
+            print("You have traveled ", miles_traveled, "miles.")
+            print("You have ", food_reserves, "food left in your reserves.")
+            print("The pirates are ", miles_traveled - dist_traveled_p, "miles behind you.")
+
+        # Player chooses to rest
+        elif choice.upper() == "D":
+            boat_condition = 0
+            print("You have made repairs to your boat.")
+            dist_traveled_p += random.randrange(7, 15)
+
+        # Player chooses to travel at full sail
+        elif choice.upper() == "C":
+            miles_turn = random.randrange(10, 21)
+            miles_traveled += miles_turn
+            print("You have traveled ", miles_turn, "miles!")
+            player_hunger += 1
+            boat_condition += random.randrange(1, 4)
+            dist_traveled_p += random.randrange(7, 15)
+            # Island
+            if not done and player_hunger < 6 and boat_condition < 8:
+                island = random.randrange(1, 21)
+                if island == 9:
+                    print("You have found an island!")
+                    food_reserves = 4
+                    player_hunger = 0
+                    boat_condition = 0
+
+        # Player chooses to travel at half sail
+        elif choice.upper() == "B":
+            miles_turn = random.randrange(5, 13)
+            miles_traveled += miles_turn
+            print("You have traveled ", miles_turn, "miles!")
+            player_hunger += 1
+            boat_condition += 1
+            dist_traveled_p += random.randrange(7, 15)
+            # Oasis
+            if not done and player_hunger < 6 and boat_condition < 8:
+                island = random.randrange(1, 21)
+                if island == 9:
+                    print("You have found an island!")
+                    canteen_drinks = 4
+                    player_thirst = 0
+                    camel_tiredness = 0
+
+        # Player chooses to drink from canteen
+        elif choice.upper() == "A":
+            if food_reserves == 0:
+                print("You have no more drinks left in your canteen!")
+            elif food_reserves > 0:
+                print("You take a drink from your canteen.")
+                food_reserves -= 1
+        print()
+
+        # Player hunger
+        if player_hunger > 4 and player_hunger <= 6:
+            print("You are hungry.")
+
+        elif player_hunger > 6:
+            print("You have died of hunger.")
+            done = True
+            game_over()
+
+        # Boat condition
+        if boat_condition > 5 and boat_condition <= 8:
+            print("Your camel is getting tired.")
+
+        elif boat_condition > 8:
+            print("Your boat has sunk!")
+            done = True
+            game_over()
+
+        # Natives distance
+        if dist_traveled_p >= miles_traveled:
+            print("The pirates have caught you!")
+            done = True
+
+        elif dist_traveled_p >= miles_traveled - 15 and dist_traveled_p < miles_traveled:
+            print("The pirates are close behind!")
+
+        # Winning the game
+        if miles_traveled >= 200:
+            print("Congratulations! You have made it across the Golden Sea!")
+            print("You win!")
+            done = True
+
+if __name__ == "__main__":
+    main()
--- Sample Programs/Practice.py
+++ Sample Programs/Practice.py
@@ -1,24 +1,5 @@
-import arcade
+import random
 
+my_number = random.random() * 5 + 10
 
-def draw_house(position_x, position_y):
-
-    arcade.draw_rectangle_filled(position_x, position_y, 100, 100, arcade.color.AERO_BLUE)
-    arcade.draw_triangle_filled(position_x - 75, position_y + 50,
-                                position_x + 75, position_y + 50,
-                                position_x, position_y + 100,
-                                arcade.color.BLACK)
-
-arcade.open_window("House", 600, 600)
-
-arcade.set_background_color(arcade.color.YELLOW)
-
-arcade.start_render()
-
-
-
-draw_house(300, 300)
-
-arcade.finish_render()
-
-arcade.run()
+print(my_number)
